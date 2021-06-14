698d38d4f05e035ab5537cee6708c73bb2113e11
hcopca <76500192+hcopca@users.noreply.github.com>
2021-06-14 00:49:46 -0500

    Ciclo while y for

--- ConceptosBasicos/src/Ciclos.java
+++ ConceptosBasicos/src/Ciclos.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Ciclos {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("¿Cuántos números quieres procesar?");
+        int cantidad = entrada.nextInt();
+
+        System.out.printf("Ingresa %d datos\n", cantidad);
+//        Este es el ciclo for
+//        for (int i = 1; i<= cantidad; i++){
+//            System.out.printf("Dato %d: ", i);
+//            int num = entrada.nextInt();
+//            int resultado = num * 2;
+//            System.out.printf("Dato %d procesado: %d\n", i, resultado);
+
+//            ESTE ES EL CICLO WHILE
+
+        int c = 1;
+        while (c <= cantidad) {
+            System.out.printf("Dato %d: ", c);
+            int num = entrada.nextInt();
+            int resultado = num * 2;
+            System.out.printf("Dato %d procesado: %d\n", c, resultado);
+            c++;
+        }
+    }
 }
