b1ebdd55ea4574c9edfbe4278d796bfcbc0082ca
hcopca <76500192+hcopca@users.noreply.github.com>
2021-06-14 09:45:48 -0500

    Optimización del código del juego

--- ConceptosBasicos/src/AdivinaElNumero.java
+++ ConceptosBasicos/src/AdivinaElNumero.java
@@ -4,35 +4,65 @@ public class AdivinaElNumero {
 
     private Scanner entrada = new Scanner(System.in);
     private boolean juegoActivo = true;
-    public void jugar (){
-        System.out.println("¿Cuál es tu nombre?");
-        String nombreJugador = entrada.nextLine();
-        System.out.printf("Bienvenido %s, vamos a comenzar\n"
-        , nombreJugador);
 
-        while (juegoActivo){
+    public void jugar() {
+        String nombreJugador = obtenerNombreJugador();
+
+        while (juegoActivo) {
             int intentos = 0;
             int min = 0;
             int max = 100;
-            int numeroJuego = (int) (Math.random() * ((max-min) + 1));
+            int numeroJuego = obtenerNumeroAleatorio(1, 100);
 
             System.out.printf("%s, He escogido un número entre %d y %d, adivinalo\nd", nombreJugador, min, max);
             int numeroJugador;
 
             do {
-                System.out.println("Escoge un numero");
-                numeroJugador = entrada.nextInt();
-                if (numeroJuego < numeroJugador) {
-                    System.out.println("Muy alto, adivina otra vez");
-                } else if (numeroJuego > numeroJugador) {
-                    System.out.println("Muy bajo, adivina otra vez");
-                }
+
+                numeroJugador = escogerNumero();
+                mensaje(numeroJuego, numeroJugador);
                 intentos++;
             } while (numeroJuego != numeroJugador);
 
             System.out.printf("Has ganado, realizaste %d intentos", intentos);
 
-            juegoActivo = false;
+            juegoActivo = jugarNuevamente();
         }
     }
-}
+
+    private int obtenerNumeroAleatorio(int min, int max) {
+
+        return min + (int) (Math.random() * ((max - min) + 1));
+    }
+
+    private String obtenerNombreJugador() {
+        System.out.println("¿Cuál es tu nombre?");
+        String nombreJugador = entrada.nextLine();
+        System.out.printf("Bienvenido %s, vamos a comenzar\n", nombreJugador);
+        return nombreJugador;
+    }
+
+    private int escogerNumero() {
+        System.out.println("Escoge un numero");
+        return entrada.nextInt();
+    }
+
+    private void mensaje(int numeroJuego, int numeroJugador) {
+        if (numeroJuego < numeroJugador) {
+            System.out.println("Muy alto, adivina otra vez");
+        } else if (numeroJuego > numeroJugador) {
+            System.out.println("Muy bajo, adivina otra vez");
+        }
+    }
+
+    public boolean jugarNuevamente() {
+        System.out.println("¿Quieres jugar nuevamente?\n1. Si\n 2. No");
+        int respuesta = entrada.nextInt();
+        if (respuesta == 1) {
+            System.out.println("Juguemos nuevamente");
+            return true;
+        } else {
+            System.out.println("Fin del juego.");
+            return false;
+        }
+    }}
